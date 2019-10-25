90ee4039d829072d99be53b79feb08a5a6f56ba8
Matej Drlicka <sprattek@gmail.com>
2019-10-25 13:15:01 +0200

    possibility to use custom route icons

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -49,8 +49,10 @@ export class MapComponent implements OnInit, OnDestroy {
   showPOI = true;
   bottomLayer = Constants.default.DEFAULT_BOTTOM_LAYER;
   lastFloorLayer = this.bottomLayer;
-  routingStartImage = null;
-  routingFinishImage = null;
+  routingStartImage = { uri: 'assets/start-point-icon.png' };
+  routingContinueImage = { uri: 'assets/continue-point-icon.png' };
+  routingFinishImage = { uri: 'assets/end-point-icon.png' };
+  useCustomRoutingImages = false;
   iconSize = 0.5;
   imagesIteration = 0;
   images = {};
@@ -115,9 +117,9 @@ export class MapComponent implements OnInit, OnDestroy {
       return acc;
     }, {});
 
-    this.routingStartImage = this.amenityBaseLinks.route_start;
 
-    if (this.routingFinishImage === null) {
+    if (!this.useCustomRoutingImages) {
+      this.routingStartImage = this.amenityBaseLinks.route_start;
       this.routingFinishImage = this.amenityBaseLinks.route_finish;
     }
 
@@ -212,6 +214,7 @@ export class MapComponent implements OnInit, OnDestroy {
     images[Constants.default.IMAGE_FLOORCHANGE_UP] = { uri: 'assets/go-up-alt.png' };
     images[Constants.default.IMAGE_FLOORCHANGE_DOWN] = { uri: 'assets/go-down-alt.png' };
     images[Constants.default.IMAGE_ROUTING_START] = this.routingStartImage;
+    images[Constants.default.IMAGE_ROUTING_CONTINUE] = this.useCustomRoutingImages ? this.routingContinueImage : this.routingFinishImage;
     images[Constants.default.IMAGE_ROUTING_FINISH] = this.routingFinishImage;
 
     this.images = images;
@@ -380,7 +383,7 @@ export class MapComponent implements OnInit, OnDestroy {
             },
             properties: {
               usecase: 'route-symbol',
-              icon: Constants.default.IMAGE_ROUTING_FINISH
+              icon: this.currentLevelChanger ? Constants.default.IMAGE_ROUTING_CONTINUE : Constants.default.IMAGE_ROUTING_FINISH
             }
           }
         ]
