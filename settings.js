c383878701fd76f5aa31f632b3e5825f078c2b03
Matej Drlicka <sprattek@gmail.com>
2020-03-19 14:26:11 +0100

    customer token

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 var Settings = {
-  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImlzcyI6IjI5NWIwYzFkLTNmYWMtNGUyZC05MWZlLTgxY2Y5MTI0ZTRiYSIsInR5cGUiOiJhcHBsaWNhdGlvbiIsImFwcGxpY2F0aW9uX2lkIjoiMDM3YWJhZTAtNWExMy00YzkyLTgxODEtZTY0YTRmNmFkYjI5In0.-nMemM6dCQyUrTBJVYDV-r0zTuZvuVfeGcCEs8h3ooo",
+  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImlzcyI6ImJmZGRiMzNmLWJhNjEtNDU4Yi1hMWVhLTI2Zjc4NmM2ZWE5YSIsInR5cGUiOiJ1c2VyIiwidXNlciI6IkthcnR0YSBLb3JvbmEiLCJ1c2VyX2lkIjoiYTI0ZjZlODYtYjhhMi00OTJmLTkyYmItMGRkYzQ1Zjk4M2RhIiwidGVuYW50X2lkIjoiYmZkZGIzM2YtYmE2MS00NThiLWExZWEtMjZmNzg2YzZlYTlhIn0.CjFTue9xBL-rmjr_R7r6oInld-nKH2P067C31XiJtiQ",
   basepath: "/wayfinding", // in case of change it's required to change that also at ./src/environments and ./angular.json files
   port: "6001", // in case of change it's required to change that also at ./src/environments
   proximi_api: 'https://api.proximi.fi'
